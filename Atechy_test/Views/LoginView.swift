eafdd4b4c5bdef8acde85cc826da102cb473dae8
Nabil Alsaadi <nabil.biloo.alsaadi95@gmail.com>
2021-05-10 22:32:09 +0400

    import fonts, icons

---
+++ Atechy_test/Views/LoginView.swift
@@ -0,0 +1,28 @@
+//
+//  LoginView.swift
+//  Atechy_test
+//
+//  Created by nabil on 5/10/21.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        VStack {
+            Image("Teamio_logo")
+                .resizable()
+                .frame(width: 150, height: 30, alignment: .center)
+            
+                
+        }
+        
+        
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
